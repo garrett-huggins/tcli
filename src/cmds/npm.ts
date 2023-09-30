520e4d0cbf562218eb02e977d9edb43f969b6a39
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2023-09-29 22:40:03 -0400

    update npm success

--- src/cmds/npm.ts
+++ src/cmds/npm.ts
@@ -1,7 +1,9 @@
 import shell from "shelljs";
 import { Spinner } from "cli-spinner";
+import { CliOptions } from "../index.js";
+import chalk from "chalk";
 
-export function npm() {
+export function npm(options: CliOptions, CURR_DIR: string) {
   const spinner = new Spinner("%s Installing dependencies...");
   spinner.setSpinnerString(18);
   spinner.start();
@@ -11,6 +13,13 @@ export function npm() {
       return;
     }
     spinner.stop(true);
-    console.log("Dependencies installed successfully!");
+    console.log(
+      chalk.green("Success! ") +
+        "Created " +
+        options.projectName +
+        " at " +
+        CURR_DIR +
+        `/${options.projectName}`
+    );
   });
 }
