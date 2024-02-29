5ccc9476de2c72a19b1a1d66658796d88a280498
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2024-02-29 16:42:44 -0500

    add vite-tw template

---
+++ templates/vite-tw/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="text-center">
+      <header className="w-full h-[40px]"></header>
+      <h1 className="text-6xl font-bold">Vite + TailwindCSS</h1>
+      <p className="text-xl">With React and TypeScript</p>
+    </main>
+  );
+}
+
+export default App;
